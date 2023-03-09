b1c8a52f8d705815ceab097cd8ba5107d70c356d
rumeiru14 <127307943+rumeiru14@users.noreply.github.com>
2023-03-09 15:07:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-#import pandas
-#import requests
+import pandas
+import requests
 import snowflake.connector
-from urllib.error import URLError
+
 
 streamlit.title('My Parents New Healthy Diner')
 
